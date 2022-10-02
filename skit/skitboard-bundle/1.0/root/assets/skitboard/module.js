8b893184e075db1bb98b3f51de024cfc486d2233
zim32 <zim32@ukr.net>
2022-10-02 23:59:18 +0300

    Misc

---
+++ skit/skitboard-bundle/1.0/root/assets/skitboard/module.js
@@ -0,0 +1,30 @@
+export default {
+
+    boot() {
+        console.log('booting app module')
+    },
+
+    // processRoutes(routes) {
+    //     routes[0].children.push({ path: '', component: () => import('./pages/DashboardPage') });
+    // },
+    //
+    // buildSidebarMenu(menu) {
+    //
+    // },
+    //
+    // processLocaleMessages(message) {
+    //
+    // },
+    //
+    // registerDataTableFields(registry) {
+    //
+    // },
+    //
+    // registerFormFields(registry) {
+    //
+    // },
+    //
+    // registerUIBuilderComponents(registry) {
+    //
+    // }
+}
